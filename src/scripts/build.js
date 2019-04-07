31fdce3466ebbbe5895c193170483543429d8aa9
Rinat Zaripov <zaripov.rinat@gmail.com>
2019-04-07 15:52:29 +1000

    chore: try to remove quotes from ignore property to fix build

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -93,7 +93,7 @@ async function babelBuild({
         ...optionalArg(
           ['--ignore'],
           doNotOutputTests
-            ? [`"${unitTestsGlob}"`, `"${integrationTestsGlob}"`]
+            ? [`${unitTestsGlob}`, `${integrationTestsGlob}`]
             : []
         ),
         ...args,
